3554ddffa67275c98f2c6b44c96c63cd31573736
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2025-02-14 10:46:04 -0500

    sample test

---
+++ someTests/add.go
@@ -0,0 +1,5 @@
+package sometests
+
+func Add(a, b int) int {
+	return a + b
+}
---
+++ someTests/add_test.go
@@ -0,0 +1,12 @@
+package sometests
+
+import "testing"
+
+func TestAddSuccess(t *testing.T) {
+	result := Add(20, 2)
+	expect := 22
+
+	if result != expect {
+		t.Errorf("got %d, exppected %d", result, expect)
+	}
+}
