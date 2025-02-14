5731a9a3d7bb6b05d0f1ab97d24ed532fc46bae5
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2025-02-14 11:41:15 -0500

    added code for go unit testing course

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/Luiggy102/go-unit-test/controller"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	port := ":8080"
+	router := mux.NewRouter()
+
+	router.HandleFunc("/pokemon/{id}", controller.GetPokemon).
+		Methods("GET")
+
+	fmt.Println("Server started at port", port)
+	if err := http.ListenAndServe(port, router); err != nil {
+		fmt.Print("Error found")
+	}
 }
