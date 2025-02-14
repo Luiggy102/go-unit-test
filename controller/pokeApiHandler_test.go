3a8e84828ef65daf4872b9e6a1a07a09dd81d6d6
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2025-02-14 16:55:39 -0500

    added Test for mocking the GetPokemons handerfunc endpoint

--- controller/pokeApiHandler_test.go
+++ controller/pokeApiHandler_test.go
@@ -3,10 +3,13 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/Luiggy102/go-unit-test/models"
+	"github.com/gorilla/mux"
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/require"
 )
@@ -52,5 +55,46 @@ func TestGetPokemonFromApiWithMocks(t *testing.T) {
 	err = json.Unmarshal([]byte(body), &expected)
 	c.NoError(err)
 
+	c.Equal(expected, result)
+}
+
+// endpont mocking
+// "/pokemon/{id}"
+// controller.GetPokemon(w http.ResponseWriter, r *http.Request)
+func TestGetPokemon(t *require.TestingT) {
+	c := require.New(t)
+
+	// Request is Get
+	r, err := http.NewRequest("GET", "/pokemon/{id}", nil)
+	c.NoError(err)
+	// ResponseWriter as new recorder
+	w := httptest.NewRecorder()
+
+	// simulate vars
+	vars := map[string]string{
+		"id": "bulbasaur",
+	}
+
+	// set the vars to the URL
+	r = mux.SetURLVars(r, vars)
+
+	// test the function
+	GetPokemon(w, r)
+
+	// expected result
+	var expected models.Pokemon
+	expectedBodyResponse, err := os.ReadFile("./samples/api_response.json")
+	c.NoError(err)
+	err = json.Unmarshal(expectedBodyResponse, &expected)
+	c.NoError(err)
+
+	// actual result (in the ResponseWriter)
+	// as json
+	var result models.Pokemon
+	err = json.Unmarshal(w.Body.Bytes(), &result)
+	c.NoError(err)
+
+	// compare
+	c.Equal(http.StatusOK, w.Code)
 	c.Equal(expected, result)
 }
